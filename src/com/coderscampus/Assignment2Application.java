a0ece9b6eae57ccd5808cdafa80b6707985fae36
Jamie Gable <146774360+jamiegable24@users.noreply.github.com>
2024-05-09 15:07:29 -0500

    Assignment 2-Attempt 2

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -8,44 +8,39 @@ public class Assignment2Application {
 	public static void main(String[] args) {
 
 		int guesses = 5;
+		boolean gameOver = false;
 		Random theRandomNumber = new Random();
 		int rndm = theRandomNumber.nextInt(101);
+		
+		System.out.println(rndm);
 
 		System.out.println("Pick a number between 1 and 100");
 		Scanner scanner = new Scanner(System.in);
-		int userGuess = scanner.nextInt();
 
-		while (userGuess < 1 || userGuess > 100) {
-			System.out.println("Your guess is not between 1 and 100, please try again: ");
-			userGuess = scanner.nextInt();
-		}
+		// scanner.close();
 
-		while (userGuess != rndm && guesses > 1) 
-			{
-			if (userGuess != rndm) {
-				System.out.println("That is not it! You have " + (guesses - 1) + (" guesses left."));
-			}
-			if (userGuess < rndm) {
-				System.out.println("Please pick a higher number: ");
-				userGuess = scanner.nextInt();
-				guesses--;
-			}
-			if (userGuess > rndm) {
-				System.out.println("Please pick a lower number: ");
-				userGuess = scanner.nextInt();
-				guesses--;
-			}
-			
+		while (!gameOver) {
+			int userGuess = scanner.nextInt();
 			if (userGuess < 1 || userGuess > 100) {
-					guesses = guesses +1;
+				System.out.println("Your guess is not between 1 and 100, please try again: ");
+			} else {
+				if (userGuess == rndm) {
+					System.out.println("You win!");
+					gameOver = true;
+				} else {
+					guesses--;
+					if (guesses == 0) {
+						System.out.println("Sorry, you lose! The correct number was: " + rndm);
+						gameOver = true;
+					} else {
+						if (userGuess < rndm) {
+							System.out.println("Please pick a higher number: ");
+						} else {
+							System.out.println("Please pick a lower number: ");
+						}
+					}
+				}
 			}
-			
-			if (userGuess == rndm) {
-				System.out.println("You win!");
-			} else if (guesses == 1) {
-				System.out.println("Sorry, you lose! The correct number was: " + rndm);
-			}
-
 		}
 	}
 }
